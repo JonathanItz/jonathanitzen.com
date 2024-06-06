dbd750aadd8a731b903d7501ecb5c349235b2fb0
JonathanItz <jonathanitzen95@gmail.com>
2024-06-06 16:14:09 -0500

    Add dark mode and other stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./**/*.{html,js}"],
+    darkMode: 'selector',
     theme: {
         extend: {
             fontFamily: {
