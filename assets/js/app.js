599fcc9795888e2a0f1fc3bc9a257c996d38f219
JonathanItz <jonathanitzen95@gmail.com>
2024-06-07 16:12:24 -0500

    JS updates and remove resume

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,23 @@
 console.log('👋')
 
 document.addEventListener('alpine:init', () => {
+    Alpine.data('sortableList', () => ({
+        items: [
+            { text: 'Laravel' },
+            { text: 'MySQL' },
+            { text: 'Salesforce Commerce Cloud' },
+            { text: 'Livewire' },
+            { text: 'AlpineJS / JavaScript / jQuery' },
+            { text: 'HTML / CSS / SASS / TailwindCSS' },
+            { text: 'PHP' },
+            { text: 'NextJS / React' },
+            { text: 'SEO' },
+            { text: 'WordPress' },
+            { text: 'NodeJS' },
+            { text: 'VS Code' },
+        ],
+    }));
+
     // Directive: x-tooltip
     Alpine.directive('tooltip', (el, { expression }) => {
         tippy(el, { content: expression, theme: 'jonathan', })
